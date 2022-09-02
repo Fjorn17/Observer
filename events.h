569c55c6263e0d9522f605d2c24d511b1a5f4f84
Fjorn17 <fjorn17@gmail.com>
2022-09-02 12:20:13 +0200

    corregir errores

--- events.h
+++ events.h
@@ -1,22 +1,31 @@
-#ifndef OBSERVER_H
-#define OBSERVER_H
+#ifndef EVENTS_H
+#define EVENTS_H
 
 #include <vector>
+#include <string>
 
 #include "observer.h"
 
-typedef Observer::Observer Observer;
+typedef std::string string;
 
+//Subject
 class Event {
 private:
 	vector<Observer> observers;
 public:
-	virtual void attach(Observer observer) = 0;
-	virtual void detach(Observer observer) = 0;
+	virtual void attach(Observer *observer) = 0;
+	virtual void detach(Observer *observer) = 0;
 	virtual void notify() = 0;
+protected:
+
 };
 
 class OnMouseDown : Event {
+public:
+	void attach(Observer* observer) override;
+	void detach(Observer* observer) override;
+	void notify() override;
+
 
 };
 
--- observer.h
+++ observer.h
@@ -2,9 +2,14 @@
 #define OBSERVER_H
 
 #include<iostream>
+#include <string>
+
+typedef std::string string;
 
 class Observer {
-
+public:
+	virtual ~Observer() {};
+	virtual void update() = 0;
 };
 
 #endif // !OBSERVER_H
